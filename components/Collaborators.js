422d633c8ecff84907c811fec9b2add26c515592
Tehilah <tkangamba@hotmail.com>
2022-07-23 08:20:38 -0500

    nav bar

--- components/Collaborators.js
+++ components/Collaborators.js
@@ -2,8 +2,8 @@ const Collaborators = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content text-center">
-                <div className="max-w-lg">
-                    <div className={"flex flex-row  items-center"}>
+                <div className="min-h-fit">
+                    <div className={"flex flex-col  items-center md:flex-row"}>
                         <h2 className={"text-5xl font-bold mb-3 text-black "}>Collaborators</h2>
                         <ul className={"ml-9 flex flex-col justify-between "}>
                             <li className={"text-4xl font-bold mb-3 text-gray-600"}>Scienze</li>
--- pages/artists/index.js
+++ pages/artists/index.js
@@ -14,6 +14,7 @@ import Navbar from "../../components/Navbar";
 import ArtistsHeader from "../../components/ArtistsHeader";
 import Footer from "../../components/Footer";
 import AlbumsAd from "../../components/AlbumsAd";
+import Collaborators from "../../components/Collaborators";
 
 export default function ArtistsPage () {
     return(
@@ -54,7 +55,7 @@ export default function ArtistsPage () {
 
             </Swiper>
             <AlbumsAd />
-
+            <Collaborators />
             <Footer />
         </div>
     )
